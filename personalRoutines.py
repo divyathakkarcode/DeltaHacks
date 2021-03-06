1295d48d799c92cfbfdb8983fef0fc0538177b97
Zain Daudi <daudizain@hotmail.com>
2021-03-06 00:27:24 -0500

    Update personalRoutines.py

--- personalRoutines.py
+++ personalRoutines.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import calendar
+# Create a plain text calendar
+c = calendar.TextCalendar(calendar.SUNDAY)
+str = c.formatmonth(2020, 3, 0, 0)
+print(str)
+
+class workoutSession:
+    def __init__(self, date, personalRoutine)
+        self.date = calendar.date
+        self.personalRoutine = personalRoutine
+
+    
+
