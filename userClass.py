3543d1830bc5e033470eceff9bc0e3b1174b5625
Divya Thakkar <thakkd2@mcmaster.ca>
2021-03-05 23:09:30 -0500

    start

---
+++ userClass.py
@@ -0,0 +1,6 @@
+class fitnessUser:
+	def __init__(self, username, age, weight, personalRoutines):
+		self.username = username
+		self.age = age
+		self.weight = weight
+		self.personalRoutines = personalRoutines
