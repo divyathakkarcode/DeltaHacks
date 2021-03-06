24fa34f26ba9e711f0ae81218752c8c5245fc6d2
Divya Thakkar <thakkd2@mcmaster.ca>
2021-03-05 23:53:09 -0500

    Update userClass.py

--- userClass.py
+++ userClass.py
@@ -1,6 +1,23 @@
+import calendar
+
+class workoutSession:
+	def __init__(self, date, personalRoutines, 
+
+
 class fitnessUser:
 	def __init__(self, username, age, weight, personalRoutines):
 		self.username = username
 		self.age = age
 		self.weight = weight
 		self.personalRoutines = personalRoutines
+	
+	def createPersonalRoutines():
+
+testUser = fitnessUser("MrKanister12", 20, 140, [ ["Pull", ["Pull Ups", "Deadlift"]], ["Push", ["Benchpress", "Pushups"]], ["Legs", ["Squats", "Lunges"]] ])
+print(testUser.username)
+print(testUser.age)
+print(testUser.weight)
+print(testUser.personalRoutines)
+print(testUser.personalRoutines[0])
+print(testUser.personalRoutines[1])
+print(testUser.personalRoutines[2])
